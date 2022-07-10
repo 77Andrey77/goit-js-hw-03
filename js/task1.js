1ac965c9e7aebe458aac41df0b5e51db8c86d3ad
Andrii Pysmennyi <andrey.ad511989@gmail.com>
2022-07-10 20:06:58 +0300

    task1

---
+++ js/task1.js
@@ -0,0 +1,29 @@
+const user = {
+  name: 'Mango',
+  age: 20,
+  hobby: 'html',
+  premium: true,
+};
+
+user.mood = 'happy';
+// console.log(user);
+
+user.hobby = 'skydiving';
+// console.log(user);
+
+user.premium = false;
+// console.log(user);
+
+
+for (const key in user) {
+  console.log(`${key}  : ${user[key]}`);
+};
+
+
+
+// const keys = Object.keys(user);
+// console.log(keys);
+
+// keys.forEach(key => {
+//   console.log(`${key}  : ${user[key]}`);
+// });
