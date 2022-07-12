3fa3dd9c2aa38cc6bea8a436bc4ecd2ff155e302
Andrii Pysmennyi <andrey.ad511989@gmail.com>
2022-07-12 23:18:24 +0300

    task5

---
+++ js/task5.js
@@ -0,0 +1,27 @@
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 2 },
+];
+
+const getAllPropValues = function (arr, prop) {
+  const prodArray = [];
+  
+  for (const ar of arr) {
+
+    if(ar[prop] !== undefined)
+    prodArray.push(ar[prop]);
+  }
+
+  return prodArray;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(getAllPropValues(products, 'name')); // ['Радар', 'Сканер', 'Дроид', 'Захват']
+
+console.log(getAllPropValues(products, 'quantity')); // [4, 3, 7, 2]
+
+console.log(getAllPropValues(products, 'category')); // []
